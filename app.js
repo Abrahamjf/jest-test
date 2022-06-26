18c968fc8e80066fc0b4285536a67bdc34f53d93
Abrahamjf <abrahamjferrer@gmail.com>
2022-06-26 17:20:06 +0000

    :pencil: Se termino el test

--- app.js
+++ app.js
@@ -5,7 +5,19 @@ const fromEuroToDollar = function(valueInEuro){
     return valueInDollar;
 }
 
+const fromDollartoYen = function(valueInDollar){
+    // convert the given valueInDollar to Yen
+    let valueInYen = valueInDollar * 106.58;
+    //return the yen value
+    return valueInYen;
+}
 
+const fromYentoPound = function(valueInYen){
+    // convert the given valueInEuro to dollars
+    let valueInPound = valueInYen * 0.006; //
+    //return the pound value
+    return valueInPound;
+}
 
 const sum =(a,b) => {
     return a + b
@@ -13,4 +25,4 @@ const sum =(a,b) => {
 
 console.log(sum(7,3))
 
-module.exports = {sum, fromEuroToDollar};
+module.exports = {sum, fromEuroToDollar, fromYentoPound,fromDollartoYen};
--- test.js
+++ test.js
@@ -20,3 +20,19 @@ test("One euro should be 1.206 dollars", function(){
     // this is the comparison for the unit test
      expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro are 1.2 dolares, then 3.5 euros should be = (3.5 * 1.2)
 })
+
+test("One dollar should be 106.58 yens", function (){
+    const {fromDollartoYen} = require('./app.js')
+
+    const yens =fromDollartoYen(3.5)
+    const expected = 3.5 * 106.58;
+    expect(fromDollartoYen(3.5)).toBe(373.03);
+})
+
+test("One yen should be 0.006 pounds", function (){
+    const {fromYentoPound} = require('./app.js')
+
+    const pounds = fromYentoPound(3.5)
+    const expected = 3.5 * 0.006;
+    expect(fromYentoPound(3.5)).toBe(0.021);
+})
